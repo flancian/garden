c0e0e45685cf9cff209017128752bdbaed3a6f1a
Flancian <0@flancia.org>
2020-10-14 09:49:59 +0200

    dash-separated-format.py update.

--- tools/dash-separated-format.py
+++ tools/dash-separated-format.py
@@ -3,9 +3,7 @@ import pathlib
 import subprocess
 
 for filename in pathlib.Path('.').glob('**/*.md'):
-    # f = open(filename)
-    print('file: %s' % filename)
     new_filename = str(filename).lower().replace(' ', '-')
-    command = ['git', 'mv', filename, new_filename]
+    command = ['git', 'mv', str(filename), new_filename]
     print('would run: %s' % command)
     # subprocess.run(command)
