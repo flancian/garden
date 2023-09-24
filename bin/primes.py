2be5adf05725151c9a653e2219695e092de6c4e2
Flancian <0@flancia.org>
2023-09-24 20:33:21 +0200

    autopushed

--- bin/primes.py
+++ bin/primes.py
@@ -49,14 +49,14 @@ class AgoraCmd(click.Command):
 
     def __call__(self, *args, **kwargs):
         try:
-            return super(ShowUsageOnMissingError, self).__call__(
+            return super(click.ShowUsageOnMissingError, self).__call__(
                 *args, standalone_mode=False, **kwargs)
         except click.MissingParameter as exc:
             exc.ctx = None
             exc.show(file=sys.stdout)
             click.echo()
             try:
-                super(ShowUsageOnMissingError, self).__call__(['--help'])
+                super(click.ShowUsageOnMissingError, self).__call__(['--help'])
             except SystemExit:
                 sys.exit(exc.exit_code)
 
