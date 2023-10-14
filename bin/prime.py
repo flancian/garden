32009ab2abcb44fed7f10428e25ff6c8060bdd65
Flancian <0@flancia.org>
2023-10-14 23:23:56 +0200

    autopushed

--- bin/prime.py
+++ bin/prime.py
@@ -8,7 +8,7 @@ import math
 import sys
 
 # I'm not proud (I am a little bit?).
-SIEVE = []
+global SIEVE
 PROOF = [] # :)
 
 def factor(n):
@@ -75,7 +75,6 @@ def prime(n):
     else:
         click.echo(f"{n} is not prime. Want proof? :)")
         click.echo("\n".join([line for line in PROOF if f'[[{n}]]' in line]))
-        echo_sieve(SIEVE)
 
 if __name__ == '__main__':
     prime()
